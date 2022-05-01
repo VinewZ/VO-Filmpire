5f72b435b19b8a4a3f9bf3290500b57316aae490
Vinicios <vinicios@pop-os.localdomain>
2022-05-01 20:12:34 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      wassupppp
+    </div>
+  )
+}
+
+export default App
